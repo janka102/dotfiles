21920f62bf975979bd33b1db56b588c3d1663e54
Jesse Smick <janka102@gmail.com>
2018-05-07 18:14:52 -0700

    Remove macOS only files from install.sh and remove option to only get one key

--- install.sh
+++ install.sh
@@ -5,21 +5,14 @@ echo '=            Setting up dotfiles             ='
 echo '=============================================='
 echo ''
 echo 'Copying dotfiles to home directory...'
-read -p 'FILES MAY GET OVERWRITTEN, are you sure [yN]? ' -n 1
+read -p 'FILES MAY GET OVERWRITTEN, are you sure [yN]? '
 echo ''
 
 if [[ "$REPLY" =~ ^[yY]$ ]]; then
   rsync --exclude .DS_Store -avh ./home/ ~/
 fi
 
-read -p 'Setup Homebrew [yN]? ' -n 1
-echo ''
-
-if [[ "$REPLY" =~ ^[yY]$ ]]; then
-  bash ./brew.sh
-fi
-
-read -p 'Setup the SKS Keyserver certificate for GPG [yN]? ' -n 1
+read -p 'Setup the SKS Keyserver certificate for GPG [yN]? '
 echo ''
 
 if [[ "$REPLY" =~ ^[yY]$ ]]; then
@@ -32,7 +25,7 @@ if [[ "$REPLY" =~ ^[yY]$ ]]; then
   curl https://sks-keyservers.net/verify_tls.php | grep -o 'The fingerprint.*</b>'
   open ~/Downloads
 
-  read -p 'Do you want to install the certificate [yN]? ' -n 1
+  read -p 'Do you want to install the certificate [yN]? '
   echo ''
 
   if [[ "$REPLY" =~ ^[yY]$ ]]; then
@@ -41,11 +34,4 @@ if [[ "$REPLY" =~ ^[yY]$ ]]; then
   fi
 else
   echo '** You should change the keyserver options in ~/.gnupg/gpg.conf'
-fi
-
-read -p 'Setup macOS defaults [yN]? ' -n 1
-echo ''
-
-if [[ "$REPLY" =~ ^[yY]$ ]]; then
-  bash ./defaults.sh
 fi
